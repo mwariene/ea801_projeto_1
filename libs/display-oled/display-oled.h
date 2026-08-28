44c61a58da787c3665465344421eeb8799f6cce5
Marina Alves Farias <marina.farias@stoneridge.com>
2026-08-28 08:54:22 -0300

    melhor organizacao do código

--- libs/display-oled/display-oled.h
+++ libs/display-oled/display-oled.h
@@ -1,12 +1,9 @@
-#ifndef DISPLAY_OLED_H
-#define DISPLAY_OLED_H
+#ifndef DISPLAY_H
+#define DISPLAY_H
 
 #include "pico/stdlib.h"
 
-// Funções simplificadas para o usuário
-void display_init(void);
-void clean_display(void);
-void write_text_display(const char *text, int x, int y);
-void update_display(void);
+// Declarar a função
+void init_display();
 
 #endif
