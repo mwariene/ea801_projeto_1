937484fb4c0fc660c7a930e713d77361de62ab74
Mariana Leister Goncalves <marileistergoncalves@gmail.com>
2026-09-06 23:05:05 -0300

    06/09 - testes para mudanca de velocidade

--- main.c
+++ main.c
@@ -19,133 +19,243 @@ typedef enum {
     state_func, // 5
     state_stop_yes, // 6
     state_stop_no, // 7
+    state_change_velocity // 8
 } State;
 
 int main(){
     stdio_init_all();
-
+    buttons_init();
+    led_matrix_init();
     display_init();
     clean_display();
     joystick_init();
 
     State current_state = state_menu;
+    uint8_t menu_position = 0; // 0 = Start, 1 = Info
 
     State previous_state = -1;
+    uint8_t previous_menu_position = 255;
+    uint8_t stop_position = 0; // 0 = Sim, 1 = Nao
+    State previous_stop_state = -1;
+    uint8_t previous_stop_position = 255;
+    bool button_released = true;
+    
+    uint8_t count_A = 0;
+    uint8_t velocity_int = 0;
+    uint8_t max_velocity = 100;
+    char velocity[4];
+    snprintf(velocity, sizeof(velocity), "%u", velocity_int);
 
     while (true) {
-        if (current_state != previous_state) {
-        clear_buffer();
-        previous_state = current_state;
-    }
+        bool button_pressed = joystick_button_pressed();
+        bool button_clicked = button_pressed && button_released;
+        button_released = !button_pressed;
+
         switch(current_state){
             case state_menu:
-                clean_display();
-                draw_text_buffer("Menu", 10,0);
-                draw_text_buffer("Start", 5, 10);
-                draw_text_buffer("Info",5,20);
-                update_display();
-                
                 uint16_t joy_y = joystick_read_y();
-                if (joy_y < 2000){
-                    current_state = state_start;
-                } else if (joy_y > 2100){
-                    current_state = state_info;
+                if (joy_y < 1800) {
+                    menu_position = 0;
+                } else if (joy_y > 2300) {
+                    menu_position = 1;
                 }
-                else if (joy_y > 2005 || joy_y < 2098){
-                    current_state = state_menu;
+
+                if (current_state != previous_state ||
+                    menu_position != previous_menu_position) {
+                    clear_buffer();
+                    draw_text_buffer("Menu", 10,0);
+                    draw_text_buffer("Start", 5, 10);
+                    draw_text_buffer("Info",5,20);
+                    draw_circle_display(0, menu_position == 0 ? 13 : 23, 2);
+                    update_display();
+                    previous_state = current_state;
+                    previous_menu_position = menu_position;
+                }
+
+                if (button_clicked) {
+                    current_state = menu_position == 0 ? state_running : state_func;
+                    sleep_ms(20);
                 }
                 break;
+
             case state_start:
-                clean_display();
+                clear_buffer();
                 draw_text_buffer("Menu", 10,0);
                 draw_text_buffer("Start", 5, 10);
                 draw_text_buffer("Info",5,20);
                 draw_circle_display(0,13,2);
                 update_display();
-                if (joystick_button_pressed()){
+                if (button_clicked){
                     current_state = state_running;
+                    sleep_ms(20);
                 } else {
                     current_state = state_menu;
                 }
                 break;
+
             case state_info:
-                clean_display();
+                clear_buffer();
                 draw_text_buffer("Menu", 10,0);
                 draw_text_buffer("Start", 5, 10);
                 draw_text_buffer("Info",5,20);
                 draw_circle_display(0,23,2);
                 update_display();
-                if (joystick_button_pressed()){
+                if (button_clicked){
                     current_state = state_func;
+                    sleep_ms(20);
                 } else {
                     current_state = state_menu;
                 }
                 break;
+
             case state_running:
-                clean_display();
+                clear_buffer();
                 draw_text_buffer("Velocidade", 0, 0);
-                draw_text_buffer("20km/h",50,10); // precisa adc a integracao com a velocidade
+                draw_text_buffer("   km/h",50,10); // precisa adc a integracao com a velocidade
                 draw_text_buffer("Velocidade Max", 0, 25);
                 draw_text_buffer("100km/h",50,35);
                 draw_text_buffer("Distancia", 0, 50);
                 draw_text_buffer("100km/h",50,60);
                 update_display();
                 // adc lógica de velocidade
-                if (joystick_button_pressed()){
+                if (button_clicked){
                     current_state = state_stop;
+                    sleep_ms(20);
+                }
+                if (gpio_get(button_A) == 0){
+                   current_state = state_change_velocity;
+                   sleep_ms(20);
+                }
+                else if (gpio_get(button_B)==0) {
+                    clear_buffer();
+                    draw_text_buffer("50",50,10);
+                    update_display();
+                    set_matrix_all(10,0,0,false,300);
                 }
                 break;
+
             case state_stop:
-                clean_display();
-                draw_text_buffer("Desligar", 10,0);
-                draw_text_buffer("Sim", 5, 10);
-                draw_text_buffer("Nao",5,20);
-                update_display();
-                
                 uint16_t joy_y_stop = joystick_read_y();
                 if (joy_y_stop < 2000){
-                    current_state = state_stop_yes;
+                    stop_position = 0;
                 } else if (joy_y_stop > 2100){
-                    current_state = state_stop_no;
+                    stop_position = 1;
                 }
-                else if (joy_y > 2005 || joy_y < 2098){
-                    current_state = state_stop;
+
+                if (current_state != previous_stop_state ||
+                    stop_position != previous_stop_position) {
+                    clear_buffer();
+                    draw_text_buffer("Desligar", 10,0);
+                    draw_text_buffer("Sim", 5, 10);
+                    draw_text_buffer("Nao",5,20);
+                    draw_circle_display(0, stop_position == 0 ? 13 : 23, 2);
+                    update_display();
+                    previous_stop_state = current_state;
+                    previous_stop_position = stop_position;
+                }
+
+                if (button_clicked) {
+                    current_state = stop_position == 0 ? state_menu : state_running;
+                    sleep_ms(20);
                 }
                 break;
+
             case state_func:
-                clean_display();
+                clear_buffer();
                 draw_text_buffer("Voltar", 5, 10);
                 draw_circle_display(0,13,2); 
-                if (joystick_button_pressed()){
+                update_display();
+                if (button_clicked){
                     current_state = state_menu;
+                    sleep_ms(20);
                 }   
                 break;
+
             case state_stop_yes:
-                clean_display();
+                clear_buffer();
                 draw_text_buffer("Desligar", 10,0);
                 draw_text_buffer("Sim", 5, 10);
                 draw_text_buffer("Nao",5,20);
                 draw_circle_display(0,13,2);
                 update_display(); 
-                if (joystick_button_pressed()){
+                if (button_clicked){
                     current_state = state_menu;
+                    sleep_ms(20);
                 }
                 else {
                     current_state = state_stop;
                 }
                 break;
+
             case state_stop_no:
-                clean_display();
+                clear_buffer();
                 draw_text_buffer("Desligar", 10,0);
                 draw_text_buffer("Sim", 5, 10);
                 draw_text_buffer("Nao",5,20);
                 draw_circle_display(0,23,2);
                 update_display(); 
-                if (joystick_button_pressed()){
+                if (button_clicked){
                     current_state = state_running;
+                    sleep_ms(20);
                 }
                 else {
                     current_state = state_stop;
+                }
+                break;
+
+            case state_change_velocity:
+                if (velocity_int > max_velocity){
+                    set_matrix_all(5,0,0,false,0);
+                    draw_text_buffer("Limite", 0, 80);
+                    draw_text_buffer("Atingido", 0, 90);
+                    draw_text_buffer("Aperte botao", 0, 110);
+                    update_display();
+
+                    if (button_clicked){
+                        set_matrix_all(0,0,0,false,0);
+                        current_state = state_start;
+                    }
+                    
+                    
+                }
+                else{
+                
+                    if (gpio_get(button_A) == 0) {
+                        velocity_int += 10;
+                        snprintf(velocity, sizeof(velocity), "%u", velocity_int);
+
+                        clear_buffer();
+                        draw_text_buffer("Velocidade", 0, 0);
+                        draw_text_buffer("   km/h",50,10);
+                        draw_text_buffer(velocity, 47, 10);
+                        draw_text_buffer("Velocidade Max", 0, 25);
+                        draw_text_buffer("100km/h",50,35);
+                        draw_text_buffer("Distancia", 0, 50);
+                        draw_text_buffer("100km/h",50,60);
+                        update_display();
+                        sleep_ms(20);
+                    }
+
+                    else if (gpio_get(button_B)==0){
+                        velocity_int -= 10;
+                        snprintf(velocity, sizeof(velocity), "%u", velocity_int);
+
+                        clear_buffer();
+                        draw_text_buffer("Velocidade", 0, 0);
+                        draw_text_buffer("   km/h",50,10);
+                        draw_text_buffer(velocity, 47, 10);
+                        draw_text_buffer("Velocidade Max", 0, 25);
+                        draw_text_buffer("100km/h",50,35);
+                        draw_text_buffer("Distancia", 0, 50);
+                        draw_text_buffer("100km/h",50,60);
+                        update_display();
+                        sleep_ms(20);
+                    }
+                }
+
+                if (button_clicked) {
+                    current_state = state_running;
+                    sleep_ms(20);
                 }
                 break;
         }
